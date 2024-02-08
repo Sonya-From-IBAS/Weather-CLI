68864224c8634f3681d2ca911e0fe9dbb2bac2c3
Leonid <Anevskyi2018@yandex.ru>
2024-02-08 16:16:14 +0400

    add package and js files

---
+++ services/api.service.js
@@ -0,0 +1,47 @@
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+import axios from 'axios';
+
+const getIcon = (icon) => {
+    switch (icon.slice(0, -1)) { 
+        case '01':
+            return '☀';
+        case '02':
+            return '⛅';
+        case '03':
+            return '☁'; 
+        case '04':
+            return '☁';
+        case '09':
+            return '🌧';
+        case '10':
+            return '🌦';
+        case '11':
+            return '🌩';
+        case '13':
+            return '❄';
+        case '50':
+            return '🌫';
+    }
+}; 
+const getWeather = async (city) => {
+    const token = await getKeyValue(TOKEN_DICTIONARY.token);
+    if (!token) {
+        throw new Error(
+            'Не задан ключ api, задайте его через комманду -t [API_KEY]'
+        );
+    }
+    const { data } = await axios.get(
+        'https://api.openweathermap.org/data/2.5/weather',
+        {
+            params: {
+                q: city,
+                appid: token,
+                lang: 'ru',
+                units: 'metric',
+            },
+        }
+    );
+    return data;
+};
+
+export { getWeather, getIcon };
